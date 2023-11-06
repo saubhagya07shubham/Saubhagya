3b7777bb4851999c95c17edc36f7d262f3bce6df
Saubhagya <saubhagya07shubham@gmail.com>
2023-11-06 15:22:04 +0530

    gf ki aur details

--- myapp/myapp/routes/index.js
+++ myapp/myapp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'saubhagya' });
+  res.render('index', { title: 'saubhagya and adista' });
 });
 
 module.exports = router;
